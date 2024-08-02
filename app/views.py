d65ace8e10000d93afaef7d196bdea0e24fdc4cb
Лёва Нерсесян <lyova123454321@gmail.com>
2024-08-02 16:19:57 +0300

    update index and views

--- app/views.py
+++ app/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    info = [
+        {
+            'Init': 'фио',
+            'GIP': f'Петров',
+            'Comment_GIP': f'Коммент',
+            'initiator': f'2012-04-01'
+        }
+    ]
+    return render(request, "index.html", {'info': info})
